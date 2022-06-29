c1e9d9654a9f1e1bce5723fd6eee335ed28ce568
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-06-29 02:56:40 -0400

    add html structure

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+
+// Function to play the actual Game
+
+// Function to change display of html cards 
+
+// Function for Timer 
+
+// For loop to create answer button for each array item 
+
+// Function to check if choice is right or wrong, then move to next question
+
+// Function to decrement timer if choice is incorrect 
+
+// Function to end game 
+
+// Function to change to highscore card
+
+// Function to add players highscore to localStorage table
+
